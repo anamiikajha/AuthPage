ec1151a88c8fce374a8d7b6a88c5ff5e0a19cd64
Anamika Jha <89740849+anamiikajha@users.noreply.github.com>
2023-09-11 00:50:20 +0545

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
@@ -11,8 +12,10 @@ loginLink.addEventListener('click', ()=> {
     wrapper.classList.remove('active');
 });
 
-
-
 btnPopup.addEventListener('click', ()=> {
-    wrapper.classList.add('active.popup');
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
 });
